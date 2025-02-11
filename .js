1454dbfba360c67e8066f11071ea1741f6010c58
Umer-Bilal991 <umerbilal1284@gmail.com>
2025-02-11 12:03:15 +0500

    Update .js

--- .js
+++ .js
@@ -12,3 +12,39 @@ console.log(newarray)
 let RemoveNum=combine.splice(3,1)
 console.log (RemoveNum)
 
+
+let colorset1=['red', 'blue']
+let colorset2=colorset1
+colorset1.push ('green')
+console.log('colorset1', colorset1)
+console.log('colorset2', colorset2)
+
+const obj = ( 'name= umer', 'name= UMer')
+console.log (obj)
+
+
+
+
+
+
+
+
+
+let product={ 
+    name: 'HP Elitebook Sleeve'
+    age:18
+    number:1000
+}
+console.log(product.'name')
+
+
+
+resturant = {
+    name: "Ramen Resturant",
+    address: Math.floor(Math.random()*500)+500
+}
+console.log(resturant)
+
+
+
+
