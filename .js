3092174b182f1fdcb9a2953f1a7e7eb8baa8f582
Umer-Bilal991 <umerbilal1284@gmail.com>
2025-02-13 11:50:17 +0500

    Update .js

--- .js
+++ .js
@@ -22,7 +22,13 @@ console.log('colorset2', colorset2)
 const obj = ( 'name= umer', 'name= UMer')
 console.log (obj)
 
+let lastname = 'Testing'
+let fullname= firstname+lastname
+console.log(fullname)
 
+
+let fulladdress='this is my ramdom number ${Math.random()*500}
+console.log(fulladdress)
 
 
 
@@ -44,6 +50,144 @@ resturant = {
     address: Math.floor(Math.random()*500)+500
 }
 console.log(resturant)
+
+
+
+let person = {
+    name: 'Ichiran Ramen',
+    age : '18',
+    job : 'teacher',
+}
+person.age = '20' //to change the above data
+console.log(person)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+student = {
+    name: 'umer',
+    age: '18',
+    subjects: 'CS',
+    isEnrolled: true
+}
+console.log(student.name)
+console.log(student.age)
+console.log(student.subjects)
+console.log(student.isEnrolled)
+student.grade='A'
+console.log(student.grade)
+
+
+
+
+
+
+
+
+
+
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: '$(Math.floor(Math.random()*100) +1) Johson Ave', 
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+console.log (restaurant)
+
+
+
+
+
+
+
+
+let myAge = 18 
+if (myAge>20){
+    console.log('Your age is less than 22')
+}
+else{
+    console.log('perfect')
+
+}
+
+
+
+
+
+
+
+let isUserLogin = true
+let isProSubscriberOrNot= true
+if (isUserLogin==false){
+    console.log('please login to see data')
+}
+else {
+
+if (isProSubscriberOrNot==false) {
+    console.log('you are pro subscriber')
+}
+else{
+    console.log('please Subscribe')
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+// || = And
+// && = Or
+// ! = Not----opposite 
+
+Ex: 
+user=false
+if(!user){
+    console.log('please login')
+}
+
+
+
+user=true
+pro=false
+if(!user){
+    console.log('please login')
+}
+if(user && pro){
+    console.log('you are a pro subscriber')
+}
+if (user && !pro){
+    console.log('please subscribe')
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
