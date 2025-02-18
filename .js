c4fede2d605e8b8424db9e141eeaa1cbe47fb8d4
Umer-Bilal991 <umerbilal1284@gmail.com>
2025-02-18 11:46:20 +0500

    Update .js

--- .js
+++ .js
@@ -406,3 +406,163 @@ if (user && !pro){
 
 
 
+
+
+
+let dayofweek = 'friday';
+
+switch (true){
+    case (dayofweek == 'saturday') ||( dayofweek == 'sunday' ):
+        console.log("wake up at 10am ")
+        break
+    case dayofweek == "monday":
+        console.log("wake up at 8am")
+        break
+    case (dayofweek == 'tuesday') || (dayofweek == 'wednesday'):
+        console.log("wake up at 9am")
+        break
+    case dayofweek == 'thursday':
+        console.log("wake up at 9:30am")
+        break
+    case dayofweek== 'friday':
+        console.log("wake up at 8:30am")
+        break
+    default :
+    console.log("no alarm set for this day")        
+}
+
+
+
+
+
+
+
+
+let Marks = 95;
+let Output = '';
+
+if (Marks >= 90) {
+    console.log("A");
+} else if (Marks >= 80) {
+    console.log("B");
+} else if (Marks >= 70) {
+    console.log("C");
+} else {
+    console.log("Fail");
+}
+
+
+
+
+
+
+
+
+Marks = 95
+
+switch (true){
+    case Marks>=90:
+        console.log("A")
+        break;
+    case ">=80":
+        console.log("B")let trafficligt = 'red';
+let message ='';
+
+switch (trafficligt){
+    case 'red':
+        console.log("stop")
+        break
+    case "yellow":
+        console.log("prepare to stop")
+        break
+    case 'green':
+        console.log("continue driving")
+        break
+    default:
+        console.log("INVALID")
+}
+
+
+        break
+    case '>=70':
+        console.log("C")
+        break
+    default:
+        console.log("Fail")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let trafficligt = 'red';
+let message ='';
+
+switch (trafficligt){
+    case 'red':
+        console.log("stop")
+        break
+    case "yellow":
+        console.log("prepare to stop")
+        break
+    case 'green':
+        console.log("continue driving")
+        break
+    default:
+        console.log("INVALID")
+}
+
+
+
+
+
+
+
+for (num =0; num<=50; num= num+5 ){
+    console.log(num)
+}
+
+
+for (n=1; n<=20; n=n+1){
+    console.log(n)
+}
+
+for (n=20; n>=1; n=n-1) {
+    console.log(n)
+}
+
+sum =0;
+for (let n=1; n<= 20; n=n+1 ){
+    sum=sum+n
+}
+console.log (sum)
+
+
+
+for (let num=1; num<=40; num=num+2){
+    if(num%5 !==0){
+    console.log(num)
+}
+}
+
+
+for (let n=1; n<=100; n=n+1){
+    if(n%10 !==0){
+        console.log("Ceckpoint")
+    }
+    console.log(n)
+}
+
+
+
+
+
